13ce112e8ce4af7c0a0b3dfbcd390e7e2cd96064
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2024-10-08 11:57:33 +0900

    Update and rename iat_hook.cpp to (origin)iat_hook.cpp

---
+++ (origin)iat_hook.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+
+BOOL WINAPI DllMain( HINSTANCE hinstDLL, DWORD  fdwReason, LPVOID lpReserved)
+{
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        g_pOrgFunc = GetProcAddress(GetModuleHandle(L"user32.dll"),"SetWindowTextW");
+        hook_iat("user32.dll", g_pOrgFunc, (PROC)ex_SetWindowTextW);
+        break;
+
+    case DLL_PROCESS_DETACH:
+        hook_iat("user32.dll", (PROC)ex_SetWindowTextW, g_pOrgFunc);
+        break;
+    }
+    return TRUE;
+}
+
+BOOL WINAPI ex_SetWindowTextW(HWND hWnd, LPWSTR lpString)
+{
+    wchar_t* pNum = L"영일이삼사오육칠팔구";
+    wchar_t temp[2] = { 0, };
+    int i = 0, nLen = 0, nIndex = 0;
+
+    nLen = wcslen(lpString);
+    for (i = 0; i < nLen; i++)
+    {
+        if (L'0' <= lpString[i] && lpString[i] <= L'9') 
+        {
+            temp[0] = lpString[i];
+            nIndex = _wtoi(temp);
+            lpString[i] = pNum[nIndex];
+        }
+    }
+    return ((PFSETWINDOWTEXTW)g_pOrgFunc)(hWnd, lpString);
+}
+
+BOOL hook_iat(LPCSTR szDllName, PROC pfnOrg, PROC pfnNew)
+{
+    HMODULE hMod;
+    LPCSTR szLibName;
+    PIMAGE_IMPORT_DESCRIPTOR pImportDesc;
+    PIMAGE_THUNK_DATA pThunk;
+    DWORD dwOldProtect, dwRVA;
+    PBYTE pAddr;
+
+    hMod = GetModuleHandle(NULL);
+    pAddr = (PBYTE)hMod;
+
+    pAddr += ((DWORD)&pAddr[0x3C]);
+
+    dwRVA = *((DWORD*)&pAddr[0x80]);
+
+    pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD)hMod + dwRVA);
+
+    for (; pImportDesc->Name; pImportDesc++) 
+    {
+        szLibName = (LPCSTR)((DWORD)hMod + pImportDesc->Name);
+
+        if (!stricmp(szLibName, szDllName))
+        {
+            pThunk = (PIMAGE_THUNK_DATA)((DWORD)hMod + pImportDesc->Name);
+
+            for (; pThunk->u1.Function; pThunk++)
+            {
+                if (pThunk->u1.Function == (DWORD)pfnOrg) 
+                {
+                    VirtualProtect((LPVOID)&pThunk->u1.Function, 4, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+                    pThunk->u1.Function = (DWORD)pfnNew;
+
+                    VirtualProtect((LPVOID)&pThunk->u1.Function, 4, dwOldProtect, &dwOldProtect);
+
+                    return TRUE;
+                }
+            }
+        }
+    }
+    return FALSE;
+}
